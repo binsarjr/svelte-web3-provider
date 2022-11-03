42d2f9751b8b5806dd43ebf0577b481bf6b1d106
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-11-03 23:50:09 +0700

    chore: rename variable

--- src/lib/store/preferences.ts
+++ src/lib/store/preferences.ts
@@ -1,11 +1,11 @@
-import { writable as writableTemp } from "svelte/store";
+import { writable  } from "svelte/store";
 import type Web3 from 'web3'
 
-export let connected = writableTemp(false)
-export let activeAddress = writableTemp<string | null>(null)
+export let connected = writable(false)
+export let activeAddress = writable<string | null>(null)
 /**
  * Please make sure connection already connected
  */
-export let web3 = writableTemp<Web3>()
-export let isLoading = writableTemp(true)
-export let isEthereumBrowser = writableTemp(true)
+export let web3 = writable<Web3>()
+export let isLoading = writable(true)
+export let isEthereumBrowser = writable(true)
